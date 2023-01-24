4810886705fddb14085d33b7d5304feff8f76923
Sherrina Robinson <robinsonsherrina@gmail.com>
2023-01-24 13:16:36 -0500

    added scrollup event

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -153,6 +153,10 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/
-
-
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    //When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scrollup class
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 /*==================== DARK LIGHT THEME ====================*/ 
